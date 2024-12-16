b013023cf71ee9d82785fb62cf691aa4e0422c81
APS4087 <aungpyaesoe.bgs@Gmail.com>
2024-12-16 13:04:24 +0800

    setting up css

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(methodOverride());
 app.use(errorHandler());
+app.use(express.static(path.join(__dirname, "public")));
 
 // Initialize Prismic client
 const initPrismicApi = (req) => {
